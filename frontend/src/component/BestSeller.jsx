e76351b6f87c746f849eccedd1fbd6e2eb3a8a36
Lauren Yeung <laurenyeung217@gmail.com>
2024-10-20 09:49:34 +0000

    Feat: Add our policy component to home pages
    - Update the styling of best sellers to improve UI

--- frontend/src/component/BestSeller.jsx
+++ frontend/src/component/BestSeller.jsx
@@ -21,7 +21,7 @@ const BestSeller = () => {
                 </p>
             </div>
             {/* Rendering BestSeller Products */}
-            <div className="grid grid-col-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
                 {
                     bestSeller.map((item, index) => (
                         <Productitem  key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
