c9c0847274092f8c97805a8b33d1d0be6d56e71d
Lauren Yeung <laurenyeung217@gmail.com>
2024-10-20 11:43:35 +0000

    Feat: Add sorting functionality to Collection page
    - Added `sortProduct` function to handle sorting logic based on selected sort type:
      - 'low-high' sorts products by price from low to high.
      - 'high-low' sorts products by price from high to low.
      - Default applies the filter without additional sorting.
    - Updated `useEffect` to trigger sorting when `sortType` changes.
    - Connected `sortType` to the select input for dynamic sorting.

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -11,6 +11,7 @@ const Collection = () => {
   const [filterProducts, setFilterProducts] = useState([]);
   const [category, setCategory] = useState([]);
   const [subCategory, setSubCategory] = useState([]);
+  const [sortType, setSortType] = useState('relavent');
 
   const toggleCategory = (e) => {
     if (category.includes(e.target.value)) {
@@ -45,9 +46,33 @@ const Collection = () => {
 
   }
 
+  const sortProduct = () => {
+
+    let fpCopy = filterProducts.slice();
+
+    switch (sortType) {
+      case 'low-high':
+        setFilterProducts(fpCopy.sort((low, high) => (low.price - high.price)));
+        break;
+
+      case 'high-low':
+        setFilterProducts(fpCopy.sort((low, high) => (high.price - low.price)));
+        break;
+
+      default:
+        applyFilter();
+        break;
+    }
+
+  }
+
   useEffect(() => {
     applyFilter();
   }, [category, subCategory])
+
+  useEffect(() => {
+    sortProduct();
+  }, [sortType])
 
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
@@ -101,7 +126,9 @@ const Collection = () => {
         <div className="flex justify-between text-base sm:text-2xl mb-4">
           <Title text1={'ALL'} text2={'COLLECTIONS'} />
           {/* Product Sort */}
-          <select className='border-2 border-gray-300 text-sm px-2'>
+          <select 
+          onChange={(e)=> setSortType(e.target.value)}
+          className='border-2 border-gray-300 text-sm px-2'>
             <option value="relavent">Sort by: Relavent</option>
             <option value="low-high">Sort by: Low to High</option>
             <option value="high-low">Sort by: High to Low</option>
