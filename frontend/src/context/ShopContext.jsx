870774a4ad83169dbe9a2e9142e07e0cad67d1d9
Lauren Yeung <laurenyeung217@gmail.com>
2024-10-23 14:27:36 +0000

    Feat: Add quantity update functionality in Cart and ShopContext
    - Added `updatedQuantity` function in ShopContext to allow quantity updates for cart items.
    - Updated Cart component to fetch `updateQuantity` from ShopContext and use it for updating item quantities.
    - Enhanced the Cart page to render items dynamically based on the cart state.
    - Added conditional input handler in Cart page for modifying item quantities or removing items.
    - Applied new styles for cart items in `index.css` to improve layout.

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -31,13 +31,24 @@ const ShopContextProvider = (props) => {
         }, 0);
     }
 
+    const updateQuantity = async (itemId, size, quantity) => {
+
+        let cartData = structuredClone(cartItems);
+        cartData[itemId][size] = quantity;
+        setCartItems(cartData);
+    }
+
     const value = {
-        products, currency, delivery_fee,
+        products,
+        currency,
+        delivery_fee,
         search, setSearch,
         showSearch, setShowSearch,
         cartItems, addToCart,
-        getCartCount
+        getCartCount,
+        updateQuantity
     }
+
 
     return (
         <ShopContext.Provider value={value}>
