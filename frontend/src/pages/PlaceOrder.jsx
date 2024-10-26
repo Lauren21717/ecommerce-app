2d5532c1a20c288a2c48fad8d088d6aa518d5d31
Lauren Yeung <laurenyeung217@gmail.com>
2024-10-26 16:21:17 +0000

    Add Cart Total, Payment method section to PlaceOrder page.

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import Title from '../component/Title';
 import CartTotal from '../component/CartTotal'
 import { assets } from '../assets/assets';
+import { ShopContext } from '../context/ShopContext';
 
 const PlaceOrder = () => {
 
   const [method, setMethod] = useState('cod');
+
+  const { navigate } = useContext(ShopContext);
 
   return (
     <div className='flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t'>
@@ -63,7 +66,50 @@ const PlaceOrder = () => {
           placeholder='Phone Number' />
       </div>
 
-      
+      {/* Right Side--CartTotal */}
+      <div className='mt-8'>
+
+        <div className='mt-8 min-w-80'>
+          <CartTotal />
+        </div>
+
+        <div className="mt-12">
+          <Title text={'PAYMENT'} text2={'METHOD'} />
+          {/* Payment Method */}
+          <div className="flex gap-3 flex-col lg:flex-row">
+
+            <div onClick={() => setMethod('stripe')} className="payment-method-container">
+              <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'stripe' ? 'bg-green-400' : ''}`}></p>
+              <img
+                className='h-5 mx-4'
+                src={assets.stripe_logo}
+                alt="Stripe Logo" />
+            </div>
+
+            <div onClick={() => setMethod('razorpay')} className="payment-method-container">
+              <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'razorpay' ? 'bg-green-400' : ''}`}></p>
+              <img
+                className='h-5 mx-4'
+                src={assets.razorpay_logo}
+                alt="Razorpay Logo" />
+            </div>
+
+            <div onClick={() => setMethod('cod')} className="payment-method-container">
+              <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'cod' ? 'bg-green-400' : ''}`}></p>
+              <p className='text-gray-500 text-sm font-medium mx-4'>CASH ON DELIVERY</p>
+            </div>
+          </div>
+
+          <div className="w-full text-end mt-8">
+            <button
+              onClick={() => navigate('/orders')}
+              className='uppercase bg-black text-white px-16 py-3 text-sm'>
+              Place order
+            </button>
+          </div>
+
+        </div>
+      </div>
 
     </div>
   )
